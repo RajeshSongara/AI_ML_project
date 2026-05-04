82b2d39ecf500c37cbf71c8d3675898bcf74aa14
Rajesh <your-email@gmail.com>
2026-05-04 20:57:37 +0530

    Initial ML project

---
+++ model/train.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+from sklearn.linear_model import LinearRegression
+import pickle
+
+data=pd.read_csv('../data/student.csv')
+
+x=data[['hours','attendance']]
+y=data['marks']
+model = LinearRegression()
+model.fit(x,y)
+pickle.dump(model,open('model.pkl','wb'))
+print("Model trained and saved")
